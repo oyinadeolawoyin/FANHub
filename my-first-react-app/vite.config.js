739c78e3f22459dc45f71f03e5e6cc39b97c03fa
oyinade olawoyin <oyinadeolawoyin@proton.me>
2025-11-18 20:58:23 +0100

    .

--- my-first-react-app/vite.config.js
+++ my-first-react-app/vite.config.js
@@ -2,23 +2,14 @@ import { defineConfig } from 'vite'
 import path from 'path';
 import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'url';
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '.', // Copy to root of dist
-        }
-      ]
-    })
-  ],
+  plugins: [react()],
+  publicDir: 'public',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
